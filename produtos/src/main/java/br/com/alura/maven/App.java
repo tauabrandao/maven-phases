c68b2df89c0edf113fbf35f3f356c85177659945
Tauã Brandão <brandao.taua@gmail.com>
2020-04-12 14:55:11 -0300

    Alteracoes no codigo de modo a testar o plugin do PMD

--- produtos/src/main/java/br/com/alura/maven/App.java
+++ produtos/src/main/java/br/com/alura/maven/App.java
@@ -4,10 +4,10 @@ package br.com.alura.maven;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		Produto produto = new Produto("bala juquinha", 0.12);
+		
+		System.out.println("Minha bala preferida é a " +produto.getNome() + " e custa " +produto.getPreco());
+	}
 }
--- produtos/src/main/java/br/com/alura/maven/Produto.java
+++ produtos/src/main/java/br/com/alura/maven/Produto.java
@@ -11,4 +11,14 @@ public class Produto {
 		this.preco = preco;
 	}
 
+	public String getNome() {
+		return nome;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+	
+	
+
 }
